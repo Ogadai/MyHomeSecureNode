510c96bcef584544c4cee8ff0700727904e04166
Andy <andy.lee.surfer@gmail.com>
2016-01-30 08:54:59 +0000

    Dsabled rfid code2 git push

--- devices/rfid.js
+++ devices/rfid.js
@@ -1,21 +1,24 @@
 ﻿var events = require('events'),
     rc522;
 
-try{
-    rc522 = require("rc522-rfid");
-}
-catch(e)
-{
-    console.error('Couldn\'t load rc522-frfid - ' + e);
-}
+//try{
+//    rc522 = require("rc522-rfid");
+//    rc522(function(rfidSerialNumber){
+//	console.log(rfidSerialNumber);
+//    });
+//}
+//catch(e)
+//{
+//    console.error('Couldn\'t load rc522-frfid - ' + e);
+//}
 
 function DeviceRFID(config) {
     var self = this;
 
     if (rc522) {
-        rc522(function(rfidSerialNumber){
-	   console.log(rfidSerialNumber);
-        });
+//        rc522(function(rfidSerialNumber){
+//	   console.log(rfidSerialNumber);
+//        });
     }
 }
 DeviceRFID.prototype.__proto__ = events.EventEmitter.prototype;
