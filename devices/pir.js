cfe2f1a77b952a4af16b7161845e1e9ff5722448
Andy <andy.lee.surfer@gmail.com>
2016-10-11 20:58:49 +0100

    added hub/node ping

--- devices/pir.js
+++ devices/pir.js
@@ -3,14 +3,23 @@
 
 function DevicePIR(config) {
     var self = this;
+    var timeout;
 
     if (Gpio) {
         var gpio = new Gpio(config.gpio.pin, 'in', 'both');
         gpio.watch(function (err, value) {
+	    if (timeout) clearTimeout(timeout);
+	    timeout = null;
+
             if (err) {
                 console.log(err);
             } else if (value === 1) {
 		self.emit('changed', 'activated');
+
+		timeout = setTimeout(function () {
+	            self.emit('changed', 'reset');
+        	    timeout = null;
+	        }, 120000);
             } else {
 		self.emit('changed', 'reset');
             }
@@ -21,7 +30,6 @@ function DevicePIR(config) {
         }
     }
 
-    var timeout;
     this._test = function () {
         if (!timeout) {
             self.emit('changed', 'activated');
--- hub-client.js
+++ hub-client.js
@@ -3,8 +3,9 @@
 
 function HubClient(socketPort) {
     var self = this,
-        client = new WebSocketClient()
-        connection = null;
+        client = new WebSocketClient(),
+        connection = null,
+	pingInterval;
     events.EventEmitter.call(this);
 
     client
@@ -15,15 +16,14 @@ function HubClient(socketPort) {
       .on('connect', function (c) {
           console.log('Connection Open');
           connection = c;
+	  pingInterval = setInterval(doPing, 10000);
 
           connection
             .on('error', function (error) {
                 console.log('Connection Error: ' + error.toString());
             })
             .on('close', function () {
-                console.log('Connection Closed. Attempting reconnect in 10 seconds');
-                connection = null;
-                setTimeout(self.connect, 10000);
+		closeConnection();
             })
             .on('message', function (message) {
                 if (message.type = 'utf8') {
@@ -34,6 +34,24 @@ function HubClient(socketPort) {
 
           self.emit('initialised');
       })
+
+	
+    function doPing() {
+	try {
+	    self.send({ method: 'ping' });
+	} catch (ex) {
+	    console.log('Error pinging node server', ex);
+	    closeConnection();
+	}
+    }
+	
+    function closeConnection() {
+	clearInterval(pingInterval);
+	
+        console.log('Connection Closed. Attempting reconnect in 10 seconds');
+        connection = null;
+        setTimeout(self.connect, 10000);
+    }
 
     this.send = function (data) {
         if (connection) {
