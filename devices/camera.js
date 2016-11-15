856b072874e1c6e84332fa2c37c948b6a9608c40
Andy Lee <alee@scottlogic.co.uk>
2016-11-15 09:20:20 +0000

    VS Code Launch Config

--- devices/camera.js
+++ devices/camera.js
@@ -1,280 +1,280 @@
 ﻿var events = require('events'),
-    extend = require('extend'),
-    fs = require('fs'),
-    http = require('http'),
-    https = require('https'),
-    RaspiCam = require('raspicam'),
-    RaspiCamMock = require('./test/raspicam-mock');
+	extend = require('extend'),
+	fs = require('fs'),
+	http = require('http'),
+	https = require('https'),
+	RaspiCam = require('raspicam'),
+	RaspiCamMock = require('./test/raspicam-mock');
 
 function DeviceCamera(config, nodeName) {
-    var self = this,
+	var self = this,
 	states = { on: { _default: 'off' } },
-        videoOn = false,
-        socketClient,
-        camera,
-        hubSettings,
-        timelapseMode = true,
-        nightMode = false,
-	    updateReq;
+		videoOn = false,
+		socketClient,
+		camera,
+		hubSettings,
+		timelapseMode = true,
+		nightMode = false,
+		updateReq;
 
-    var FILE_PATH = '../garageCam/',
-        FILE_NAME = 'snapshot%06d.jpg';
+	var FILE_PATH = '../garageCam/',
+		FILE_NAME = 'snapshot%06d.jpg';
 
-    self.applySettings = function (s) {
-        hubSettings = s;
-    };
+	self.applySettings = function (s) {
+		hubSettings = s;
+	};
 
-    self.setState = function (state) {
-        var index = state.indexOf('.');
+	self.setState = function (state) {
+		var index = state.indexOf('.');
 
-        if (index !== -1) {
-            var name = state.substring(0, index),
-                value = state.substring(index + 1);
+		if (index !== -1) {
+			var name = state.substring(0, index),
+				value = state.substring(index + 1);
 
-            states.on[name] = value;
-        } else if (state == 'night') {
-            nightMode = true;
-        } else if (state == 'day') {
-            nightMode = false;
-	} else {
-	   states.on._default = state;
+			states.on[name] = value;
+		} else if (state == 'night') {
+			nightMode = true;
+		} else if (state == 'day') {
+			nightMode = false;
+	    } else {
+	       states.on._default = state;
+	    }
+
+		var onValue = 'off';
+		for (var name in states.on) {
+			if (states.on.hasOwnProperty(name)) {
+		if (onValue === 'off') {
+					onValue = states.on[name];
+		}
+			}
+		}
+
+		if (onValue === 'timelapse') {
+			start(true);
+		} else if (onValue === 'h264') {
+			start(false);
+		} else { 
+			stop();
+		}
 	}
 
-        var onValue = 'off';
-        for (var name in states.on) {
-            if (states.on.hasOwnProperty(name)) {
-		if (onValue === 'off') {
-                    onValue = states.on[name];
+	function start(timelapse) {
+		if (!videoOn) {
+			timelapseMode = timelapse;
+			startCamera();
+			videoOn = true;
+			self.emit('changed', timelapseMode ? 'timelapse' : 'h264');
 		}
-            }
-        }
+	}
+	function stop() {
+		stopCamera();
 
-        if (onValue === 'timelapse') {
-            start(true);
-        } else if (onValue === 'h264') {
-            start(false);
-        } else { 
-            stop();
-        }
-    }
+		if (videoOn) {
+			videoOn = false;
+			self.emit('changed', 'off');
+		}
+	}
 
-    function start(timelapse) {
-        if (!videoOn) {
-            timelapseMode = timelapse;
-            startCamera();
-            videoOn = true;
-            self.emit('changed', timelapseMode ? 'timelapse' : 'h264');
-        }
-    }
-    function stop() {
-        stopCamera();
+	function startCamera() {
+		var settings = getCameraSettings();
 
-        if (videoOn) {
-            videoOn = false;
-            self.emit('changed', 'off');
-        }
-    }
+		camera = config.mock ? new RaspiCamMock(settings) : new RaspiCam(settings);
 
-    function startCamera() {
-        var settings = getCameraSettings();
+		camera.on("start", function (err, timestamp, stream) {
+			console.log((timelapseMode ? 'timelapse' : 'video') + ' started');
+			if (!timelapseMode) {
+				uploadStream(stream);
+			}
+		});
 
-        camera = config.mock ? new RaspiCamMock(settings) : new RaspiCam(settings);
+		camera.on("exit", function () {
+			camera = null;
+			console.log((timelapseMode ? 'timelapse' : 'video') + ' finished');
+			stop();
+		});
 
-        camera.on("start", function (err, timestamp, stream) {
-            console.log((timelapseMode ? 'timelapse' : 'video') + ' started');
-	        if (!timelapseMode) {
-	    	    uploadStream(stream);
-	        }
-        });
+		if (timelapseMode) {
+			var uploadingFile = false;
 
-        camera.on("exit", function () {
-            camera = null;
-            console.log((timelapseMode ? 'timelapse' : 'video') + ' finished');
-            stop();
-        });
+			camera.on("read", function (err, timestamp, filename) {
+				var filePath = FILE_PATH + filename;
 
-        if (timelapseMode) {
-            var uploadingFile = false;
+				if (fs.existsSync(filePath)) {
+				  if (uploadingFile) {
+					console.log('skipping snapshot: ' + filename);
+					removeFile(filePath);
+				} else {
+					console.log("uploading snapshot: " + filename);
+					uploadingFile = true;
 
-            camera.on("read", function (err, timestamp, filename) {
-                var filePath = FILE_PATH + filename;
+					uploadFile(filePath, function () {
+						removeFile(filePath);
+						uploadingFile = false;
+					});
+				  }
+				}
+			});
+		}
 
-		        if (fs.existsSync(filePath)) {
-                  if (uploadingFile) {
-                    console.log('skipping snapshot: ' + filename);
-                    removeFile(filePath);
-                } else {
-                    console.log("uploading snapshot: " + filename);
-                    uploadingFile = true;
+		camera.start();
+	}
+	function stopCamera() {
+		if (camera) {
+			try {
+				camera.stop();
+			} catch (ex) {
+				console.error('error stopping camera - ' + ex);
+			}
 
-                    uploadFile(filePath, function () {
-                        removeFile(filePath);
-                        uploadingFile = false;
-                    });
-                  }
-		        }
-            });
-        }
-
-        camera.start();
-    }
-    function stopCamera() {
-        if (camera) {
-            try {
-                camera.stop();
-            } catch (ex) {
-                console.error('error stopping camera - ' + ex);
-            }
-
-	    if (timelapseMode) {
+		if (timelapseMode) {
 		try {
-		    var files = fs.readdirSync(FILE_PATH);
-		    files.forEach(function(f) {
+			var files = fs.readdirSync(FILE_PATH);
+			files.forEach(function(f) {
 			console.log('removing file - ' + f);
 			try {
 			  fs.unlinkSync(FILE_PATH + f);
 			} catch(e) {
-			    console.log('error deleting file - ' + e);
+				console.log('error deleting file - ' + e);
 			}
-		    });
+			});
 		} catch(e) {
-		    console.error('error deleting files - ' + e);
+			console.error('error deleting files - ' + e);
 		}
-	    }
-        }
-        if (updateReq) {
-            try {
-                updateReq.end();
-            } catch (ex) {
-                console.error('error ending upload request - ' + ex);
-            }
-	    }
-	    camera = null;
-	    updateReq = null;
-    }
+		}
+		}
+		if (updateReq) {
+			try {
+				updateReq.end();
+			} catch (ex) {
+				console.error('error ending upload request - ' + ex);
+			}
+		}
+		camera = null;
+		updateReq = null;
+	}
 
-    function removeFile(filePath) {
-        try {
-            fs.unlinkSync(filePath);
-        } catch (ex) {
-            console.error('error deleting snapshot file - ' + ex);
-            stop();
-        }
-    }
+	function removeFile(filePath) {
+		try {
+			fs.unlinkSync(filePath);
+		} catch (ex) {
+			console.error('error deleting snapshot file - ' + ex);
+			stop();
+		}
+	}
 
-    function beginUpload() {
+	function beginUpload() {
 	var controller = timelapseMode ? 'UploadSnapshot' : 'UploadStream',
-            fullUrl = hubSettings.addr.replace('wss://', '').replace('ws://', '') + controller,
-            pathIndex = fullUrl.indexOf('/'),
-	    protocol = hubSettings.addr.substring(0, 4) == 'wss:' ? https : http,
-            host = fullUrl.substring(0, pathIndex),
-            path = fullUrl.substring(pathIndex),
-            query = '?hub=' + encodeURIComponent(hubSettings.identification.name) +
-                    '&token=' + encodeURIComponent(hubSettings.identification.token) +
-                    '&node=' + encodeURIComponent(nodeName);
+			fullUrl = hubSettings.addr.replace('wss://', '').replace('ws://', '') + controller,
+			pathIndex = fullUrl.indexOf('/'),
+		protocol = hubSettings.addr.substring(0, 4) == 'wss:' ? https : http,
+			host = fullUrl.substring(0, pathIndex),
+			path = fullUrl.substring(pathIndex),
+			query = '?hub=' + encodeURIComponent(hubSettings.identification.name) +
+					'&token=' + encodeURIComponent(hubSettings.identification.token) +
+					'&node=' + encodeURIComponent(nodeName);
 
-        var portIndex = host.indexOf(':'),
-            port = undefined;
-        if (portIndex !== -1) {
-            port = parseInt(host.substring(portIndex + 1));
-            host = host.substring(0, portIndex);
-        }
-        var options = {
-            host: host,
-            port: port,
-            path: path + query,
-            method: 'POST'
-        };
+		var portIndex = host.indexOf(':'),
+			port = undefined;
+		if (portIndex !== -1) {
+			port = parseInt(host.substring(portIndex + 1));
+			host = host.substring(0, portIndex);
+		}
+		var options = {
+			host: host,
+			port: port,
+			path: path + query,
+			method: 'POST'
+		};
 
-	    updateReq = protocol.request(options, function (res) {
-            if (res.statusCode !== 200) {
-                console.log('STATUS: ' + res.statusCode);
-                res.setEncoding('utf8');
-                res.on('data', function (chunk) {
-                    console.log('BODY: ' + chunk);
-                });
-            }
-        });
-        updateReq.on('error', function (err) {
-            console.error('error uploading: ' + err.message);
-        });
-    }
+		updateReq = protocol.request(options, function (res) {
+			if (res.statusCode !== 200) {
+				console.log('STATUS: ' + res.statusCode);
+				res.setEncoding('utf8');
+				res.on('data', function (chunk) {
+					console.log('BODY: ' + chunk);
+				});
+			}
+		});
+		updateReq.on('error', function (err) {
+			console.error('error uploading: ' + err.message);
+		});
+	}
 
-    function uploadStream(stream) {
-        try {
-            beginUpload();	
+	function uploadStream(stream) {
+		try {
+			beginUpload();	
 
-            stream.on('data', function (data) {
-                try {
-                    updateReq.write(data);
-                } catch (ex) {
-                    console.error('Error uploading stream data - ' + ex);
-                    stop();
-                }
-            });
+			stream.on('data', function (data) {
+				try {
+					updateReq.write(data);
+				} catch (ex) {
+					console.error('Error uploading stream data - ' + ex);
+					stop();
+				}
+			});
 
-            stream.on('end', function () {
-                try {
-                    console.log('end upload');
-                    updateReq.end();
-                } catch (ex) {
-                    console.error('Error ending upload of stream data - ' + ex);
-                }
-                updateReq = null;
-                stop();
-            });
-        } catch (ex) {
-            console.error('Error starting upload of stream data - ' + ex);
-            stop();
-        }
-    }
+			stream.on('end', function () {
+				try {
+					console.log('end upload');
+					updateReq.end();
+				} catch (ex) {
+					console.error('Error ending upload of stream data - ' + ex);
+				}
+				updateReq = null;
+				stop();
+			});
+		} catch (ex) {
+			console.error('Error starting upload of stream data - ' + ex);
+			stop();
+		}
+	}
 
-    function uploadFile(filePath, done) {
-        try {
-            beginUpload();
+	function uploadFile(filePath, done) {
+		try {
+			beginUpload();
 
-            fs.readFile(filePath, function (err, data) {
-                try {
-                    if (err) {
-                        console.error('error reading snapshot file - ' + err);
-                    } else {
-                        updateReq.write(data);
-                    }
-                    updateReq.end(done);
+			fs.readFile(filePath, function (err, data) {
+				try {
+					if (err) {
+						console.error('error reading snapshot file - ' + err);
+					} else {
+						updateReq.write(data);
+					}
+					updateReq.end(done);
 		
-                } catch (ex) {
-                    console.error('Error uploading snapshot file "' + filePath + '" - ' + ex);
-		    done()
-                }
-                updateReq = null;
-            });
-        } catch (ex) {
-            console.error('Error starting upload of snapshot data - ' + ex);
-            stop();
-        }
-    }
+				} catch (ex) {
+					console.error('Error uploading snapshot file "' + filePath + '" - ' + ex);
+			done()
+				}
+				updateReq = null;
+			});
+		} catch (ex) {
+			console.error('Error starting upload of snapshot data - ' + ex);
+			stop();
+		}
+	}
 
-    function getCameraSettings() {
-        var camSettings = timelapseMode
-	    ? extend({
-        	mode: "timelapse",
-        	output: FILE_PATH + FILE_NAME
-            }, config.timelapseSettings)
-	    : extend({
-        	mode: "video",
-        	output: "-"
-        }, config.videoSettings);
+	function getCameraSettings() {
+		var camSettings = timelapseMode
+		? extend({
+			mode: "timelapse",
+			output: FILE_PATH + FILE_NAME
+			}, config.timelapseSettings)
+		: extend({
+			mode: "video",
+			output: "-"
+		}, config.videoSettings);
 
-        return extend({
-            exposure: nightMode ? 'night' : 'auto',
-            awb: nightMode ? 'incandescent' : 'shade',
-	    drc: nightMode ? 'high' : 'off'
-        }, camSettings, config.settings);
-    }
+		return extend({
+			exposure: nightMode ? 'night' : 'auto',
+			awb: nightMode ? 'incandescent' : 'shade',
+		drc: nightMode ? 'high' : 'off'
+		}, camSettings, config.settings);
+	}
 
-    this._test = function () {
-        self.setState(videoOn ? "off" : "timelapse");
-    };
+	this._test = function () {
+		self.setState(videoOn ? "off" : "timelapse");
+	};
 }
 DeviceCamera.prototype.__proto__ = events.EventEmitter.prototype;
 module.exports = DeviceCamera;
--- hub-client.js
+++ hub-client.js
@@ -5,49 +5,49 @@ function HubClient(socketServer, socketPort) {
     var self = this,
         client = new WebSocketClient(),
         connection = null,
-	pingInterval;
+        pingInterval;
     events.EventEmitter.call(this);
 
     client
-      .on('connectFailed', function (error) {
-          console.log('Connect Error: ' + error.toString() + '. Attempting reconnect in 10 seconds');
-          setTimeout(self.connect, 10000);
-      })
-      .on('connect', function (c) {
-          console.log('Connection Open');
-          connection = c;
-	  pingInterval = setInterval(doPing, 10000);
+        .on('connectFailed', function (error) {
+            console.log('Connect Error: ' + error.toString() + '. Attempting reconnect in 10 seconds');
+            setTimeout(self.connect, 10000);
+        })
+        .on('connect', function (c) {
+            console.log('Connection Open');
+            connection = c;
+            pingInterval = setInterval(doPing, 10000);
 
-          connection
-            .on('error', function (error) {
-                console.log('Connection Error: ' + error.toString());
-            })
-            .on('close', function () {
-		closeConnection();
-            })
-            .on('message', function (message) {
-                if (message.type = 'utf8') {
-                    var data = JSON.parse(message.utf8Data);
-                    self.emit(data.method, data);
-                }
-            })
+            connection
+                .on('error', function (error) {
+                    console.log('Connection Error: ' + error.toString());
+                })
+                .on('close', function () {
+                    closeConnection();
+                })
+                .on('message', function (message) {
+                    if (message.type = 'utf8') {
+                        var data = JSON.parse(message.utf8Data);
+                        self.emit(data.method, data);
+                    }
+                })
 
-          self.emit('initialised');
-      })
+            self.emit('initialised');
+        })
 
-	
+
     function doPing() {
-	try {
-	    self.send({ method: 'ping' });
-	} catch (ex) {
-	    console.log('Error pinging node server', ex);
-	    closeConnection();
-	}
+        try {
+            self.send({ method: 'ping' });
+        } catch (ex) {
+            console.log('Error pinging node server', ex);
+            closeConnection();
+        }
     }
-	
+
     function closeConnection() {
-	clearInterval(pingInterval);
-	
+        clearInterval(pingInterval);
+
         console.log('Connection Closed. Attempting reconnect in 10 seconds');
         connection = null;
         setTimeout(self.connect, 10000);
@@ -55,8 +55,8 @@ function HubClient(socketServer, socketPort) {
 
     this.send = function (data) {
         if (connection) {
-	    connection.sendUTF(JSON.stringify(data));
-	}
+            connection.sendUTF(JSON.stringify(data));
+        }
     }
 
     this.connect = function () {
